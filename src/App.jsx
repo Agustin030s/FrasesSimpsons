ce08a52c6db76a03c190035716a12a75ffad8946
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-27 12:20:00 -0300

    se agrego el maquetado de la card de la frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container } from "react-bootstrap";
+import logo from "./assets/logosimpson.png";
+import Frase from "./components/Frase";
+import './App.css'
 
 function App() {
   return (
     <>
-    <h1 className='text-center'>Frase Simpsons</h1>
+      <Container className="my-5">
+        <div className="text-center">
+          <img src={logo} alt="Logo de los simpsons" className="img-fluid" />
+        </div>
+        <Frase></Frase>
+        <div className="text-center">
+        <Button className="btnFrase">Obtener Frase</Button>
+        </div>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
