3d3fc4ffd2a1e53c1f3e1f0b04add957afe3ca37
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-28 11:34:23 -0300

    agregamos componentes condicionales

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import logo from "./assets/logosimpson.png";
 import Frase from "./components/Frase";
 import "./App.css";
@@ -7,30 +7,42 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarFrase, setMostrarFrase] = useState(false);
 
   useEffect(() => {
     consultarAPI();
   }, []);
 
   const consultarAPI = async () => {
+    setMostrarFrase(false);
     try {
       const respuesta = await fetch(
         "https://thesimpsonsquoteapi.glitch.me/quotes"
       );
       const dato = await respuesta.json();
-      console.log(dato);
       setPersonaje(dato[0]);
+      setMostrarFrase(true);
     } catch (error) {
-      console.log('Hubo un error intente más tarde', error);
+      console.log("Hubo un error intente más tarde", error);
     }
   };
+
+  const mostrarComponente = mostrarFrase ? (
+    <Frase personaje={personaje}></Frase>
+  ) : (
+    <div className="my-5">
+      <Spinner animation="grow" variant="warning" />
+    </div>
+  );
 
   return (
     <>
       <Container className="my-5 text-center">
         <img src={logo} alt="Logo de los simpsons" className="img-fluid" />
-        <Frase personaje={personaje}></Frase>
-        <Button className="btnFrase">Obtener Frase</Button>
+        {mostrarComponente}
+        <Button className="btnFrase" onClick={consultarAPI}>
+          Obtener Frase
+        </Button>
       </Container>
     </>
   );
