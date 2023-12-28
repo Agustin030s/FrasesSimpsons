7b3bf3db3e513481b387c8ae8cbb497a2a8dd30e
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-28 10:41:09 -0300

    agregamos la solicitud a la api de los simpsons

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Container } from "react-bootstrap";
 import logo from "./assets/logosimpson.png";
 import Frase from "./components/Frase";
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async() =>{
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const dato = await respuesta.json();
+    console.log(dato);
+    console.log(respuesta);
+    setPersonaje(dato[0]);
+  }
+
   return (
     <>
-      <Container className="my-5">
-        <div className="text-center">
-          <img src={logo} alt="Logo de los simpsons" className="img-fluid" />
-        </div>
+      <Container className="my-5 text-center">
+        <img src={logo} alt="Logo de los simpsons" className="img-fluid" />
         <Frase></Frase>
-        <div className="text-center">
         <Button className="btnFrase">Obtener Frase</Button>
-        </div>
       </Container>
     </>
   );
